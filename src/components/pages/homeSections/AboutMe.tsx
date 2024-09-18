c9a67092e13fd32c7438813067019c006e7f61b7
shabdandev <arslanbekovshabdan1@gmail.com>
2024-09-18 20:29:52 +0600

    ..

--- src/components/pages/homeSections/AboutMe.tsx
+++ src/components/pages/homeSections/AboutMe.tsx
@@ -20,12 +20,12 @@ const AboutMe = () => {
                 <FiUser />
               </span>
               <p>
-                About Fully committed to the philosophy of life-long learning,
-                I’m a full stack developer with a deep passion for JavaScript,
+                About Fully committed to the philosophy of life-long learning, I
+                m a full stack developer with a deep passion for JavaScript,
                 React and all things web development. The unique combination of
                 creativity, logic, technology and never running out of new
                 things to discover, drives my excitement and passion for web
-                development. When I’m not at my computer I like to spend my time
+                development. When I m not at my computer I like to spend my time
                 reading, keeping fit and playing guitar.
               </p>
               <button>Submit</button>
